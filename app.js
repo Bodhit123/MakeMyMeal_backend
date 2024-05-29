3d66b1a75898c7ab4850200bb04c066089bd752a
Bodhit <dhvandarji@gmail.com>
2024-05-29 20:58:00 +0530

    created app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const {
+  signUpValidationMiddleware,
+  loginValidationMiddleware,
+} = require("./middlewares/validation.js");
+const bookingRoute = require("./routes/Booking.route.js");
+const loginRoute = require("./routes/Login.Route");
+const logoutRoute = require("./routes/logout.route");
+const SignupRoute = require("./routes/Signup.Route");
+const errorMiddleware = require("./middlewares/errorhandler");
+const changePasswordRoute = require("./routes/changePassword.route.js");
+const employeeRoute = require("./routes/Employee.route.js");
+const checkUserAuth = require("./middlewares/auth-middleware.js");
+const cors = require("cors");
+const helmet = require("helmet");
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false, // Disable contentSecurityPolicy to avoid conflicts
+    hsts: {
+      maxAge: 31536000, // 1 year
+      includeSubDomains: true,
+      preload: true,
+    },
+  })
+);
+
+app.use(cors());
+app.use(express.json());
+/*---routes---*/
+app.use("/change/password", checkUserAuth, changePasswordRoute);
+app.use("/booking", checkUserAuth, bookingRoute);
+app.use("/employee", checkUserAuth, employeeRoute);
+app.use("/login", loginValidationMiddleware, loginRoute);
+app.use("/signup", signUpValidationMiddleware, SignupRoute);
+app.use("/logout", logoutRoute);
+app.use(errorMiddleware);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,46 +1,10 @@
-const express = require("express");
-const app = express();
-const helmet = require("helmet");
-const cors = require("cors");
-const {
-  signUpValidationMiddleware,
-  loginValidationMiddleware,
-} = require("./middlewares/validation.js");
-const bookingRoute = require("./routes/Booking.route.js");
-const loginRoute = require("./routes/Login.Route");
-const logoutRoute = require("./routes/logout.route");
-const SignupRoute = require("./routes/Signup.Route");
-const ConnectDb = require("./config/connection");
+const app = require("./app");
 const dotenv = require("dotenv");
-const errorMiddleware = require("./middlewares/errorhandler");
-const changePasswordRoute = require("./routes/changePassword.route.js");
-const employeeRoute = require("./routes/Employee.route.js");
-const checkUserAuth = require("./middlewares/auth-middleware.js");
-
-ConnectDb();
+const ConnectDb = require("./config/connection");
 
 dotenv.config({ path: "./config/config.env" });
 
-app.use(
-  helmet({
-    contentSecurityPolicy: false, // Disable contentSecurityPolicy to avoid conflicts
-    hsts: {
-      maxAge: 31536000, // 1 year
-      includeSubDomains: true,
-      preload: true,
-    },
-  })
-);
-
-app.use(cors());
-app.use(express.json());
-app.use("/change/password", checkUserAuth, changePasswordRoute);
-app.use("/booking",checkUserAuth, bookingRoute);
-app.use("/employee", checkUserAuth, employeeRoute);
-app.use("/login", loginValidationMiddleware, loginRoute);
-app.use("/signup", signUpValidationMiddleware, SignupRoute);
-app.use("/logout", logoutRoute);
-app.use(errorMiddleware);
+ConnectDb();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on ${process.env.PORT}`);
